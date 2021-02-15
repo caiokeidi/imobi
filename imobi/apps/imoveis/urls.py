ee93a49fa7ec8b66b090bcb916120a128f69ef1d
CaioKeidi <caiokeidi@gmail.com>
2021-02-15 19:25:31 -0300

    add: Cadastrar Imoveis Page

--- imobi/apps/imoveis/urls.py
+++ imobi/apps/imoveis/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('imovel/<int:imovel_id>', views.imovel, name='imovel'),
     path('imovel/busca', views.busca, name='busca'),
-    path('imovel/cadastro', views.cadastro_imoveis, name='cadastro_imoveis')
+    path('imovel/cadastro', views.cadastro_imoveis, name='cadastro_imoveis'),
+    path('imovel/editar/<int:imovel_id>', views.editar_imovel, name='editar_imovel')
 ]
--- imobi/apps/imoveis/views.py
+++ imobi/apps/imoveis/views.py
@@ -153,6 +153,83 @@ def cadastro_imoveis(request):
     else:
         return redirect('login')
 
+def editar_imovel(request, imovel_id):
+
+    if request.method=='POST':
+        v_aluguel = request.POST['valor_aluguel']
+        v_venda = request.POST['valor_venda']
+        v_iptu = request.POST['valor_iptu']
+        v_condominio = request.POST['valor_condominio']
+
+        corretor = get_object_or_404(User, pk=request.user.id)
+        cliente = request.POST['cliente']
+        cidade = request.POST['cidade']
+        bairro = request.POST['bairro']
+        rua = request.POST['rua']
+        numero = request.POST['numero']
+        complemento = request.POST['complemento']
+        tipo_negocio = request.POST['tipo_negocio']
+        tipo_imovel = request.POST['tipo_imovel']
+        valor_aluguel = float(v_aluguel.replace(',', '.'))
+        valor_venda = float(v_venda.replace(',', '.'))
+        valor_iptu = float(v_iptu.replace(',', '.'))
+        valor_condominio = float(v_condominio.replace(',', '.'))
+        area = request.POST['area']
+        quartos = request.POST['quartos']
+        suites = request.POST['suites']
+        banheiros = request.POST['banheiros']
+        vagas = request.POST['vagas']
+        andar = request.POST['andar']
+
+        metro_proximo = False
+        if 'metro_proximo' in request.POST:
+            metro_proximo = True
+
+        mobiliado = False
+        if 'mobiliado' in request.POST:
+            mobiliado = True
+
+        descricao = request.POST['descricao']
+
+        publicado = False
+        if 'publicado' in request.POST:
+            publicado = True
+
+
+        imovel=Imoveis.objects.create(corretor=corretor, cliente=cliente, cidade=cidade, bairro=bairro, rua=rua,
+        numero=numero, complemento=complemento, tipo_negocio=tipo_negocio, tipo_imovel=tipo_imovel, valor_aluguel=valor_aluguel,
+        valor_venda=valor_venda, valor_iptu=valor_iptu, valor_condominio=valor_condominio, area=area, quartos=quartos,
+        suites=suites, banheiros=banheiros, vagas=vagas, andar=andar, metro_proximo=metro_proximo, mobiliado=mobiliado,
+        descricao=descricao, publicado=publicado)
+
+        #imovel.save()
+        
+        #imgs = request.FILES.getlist('imagens')
+        #for img in imgs:
+            #imagens.objects.create(foto=img, imoveis=imovel)
+        
+
+        return render(request, 'imoveis/cadastro.html')    
+
+    else:
+        imovel = get_object_or_404(Imoveis, pk=imovel_id)
+        imgs = imagens.objects.filter(imoveis=imovel_id)
+        imgs_and_thumbs = []
+        for i in imgs:
+            ###Precisei colocar o replace(media) pois por algum motivo ele estava dando media duplicado no url
+            element = {}
+            thumbnail = get_thumbnailer(settings.MEDIA_ROOT.replace('\media', '')+i.foto.url)['miniatura']
+            element['thumb'] = thumbnail
+            element['img'] = i
+
+            imgs_and_thumbs.append(element)
+
+        imovel_a_exibir = {
+            'imovel': imovel,
+            'imagens': imgs_and_thumbs
+        }
+
+        return render(request, 'imoveis/editar_imovel.html', imovel_a_exibir)
 
 
 def cidade_bairro():
